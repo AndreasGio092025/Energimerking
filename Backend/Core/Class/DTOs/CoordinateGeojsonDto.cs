c89e936cf8938e6dbc1b5097bb20051f2de26c9b
Jarand220925 <jarandkurs@gmail.com>
2026-03-26 10:15:09 +0100

    Lagt til Serialiserer

--- Backend/Core/Class/DTOs/CoordinateGeojsonDto.cs
+++ Backend/Core/Class/DTOs/CoordinateGeojsonDto.cs
@@ -23,4 +23,5 @@ public class CoordinateGeojsonDto
         
         feature = new(point, attributes);
     }
+    
 }
---
+++ Backend/Core/Class/DTOs/GeojsonSerializer.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using NetTopologySuite.Features;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO.Converters;
+
+namespace ef_core_migration_test.Models.DTOs;
+
+public class GeojsonSerializer
+{
+    public string Json;
+
+    public GeojsonSerializer(List<CoordinateGeojsonDto> coordinateGeojsonDtos)
+    {
+        List<Feature> featureList = coordinateGeojsonDtos.Select(coordGeojsonDto => coordGeojsonDto.feature).ToList();
+        var featureCollection = new FeatureCollection();
+        featureList.ForEach(feature => featureCollection.Add(feature));
+        
+        var options = new JsonSerializerOptions
+        {
+            Converters = { new GeoJsonConverterFactory() },
+            // Optional: for pretty formatting
+            WriteIndented = true 
+        };
+        Json = JsonSerializer.Serialize(featureCollection, options);
+    }
+}
