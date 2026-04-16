96a32960e599a3b95b6ca9b9acd5776b9ec5ffe6
Jarand220925 <jarandkurs@gmail.com>
2026-04-16 13:49:09 +0200

    La til dokumentasjon på GeojsonSerializer.cs og IGeojsonDto.cs. Fikset potensielt CoordinateGeojsonDto.cs.

--- Backend/Core/Interface/IGeojsonDto.cs
+++ Backend/Core/Interface/IGeojsonDto.cs
@@ -1,7 +1,9 @@
 ﻿using NetTopologySuite.Features;
 
 namespace Core.Interface;
-
+/// <summary>
+/// Lagrer Feature-en som skal brukes i GeojsonSerializer.
+/// </summary>
 public interface IGeojsonDto
 {
     public Feature feature { get; set; }
